619d0fa3e9bca7627a66461a6f7384695095990f
Nicholas Karsant <nkarsant@gmail.com>
2021-02-03 17:08:11 -0800

    connect client submodule to project

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -7,4 +7,8 @@ router.get('/', function(req,res,next){
   res.send('API working properly');
 });
 
+router.get('/yelpcamp', function(req,res,next){
+  res.send('yelpcamp landing here');
+});
+
 module.exports = router;
